e275758e72e0af73632ceb6897a2f59cfbf30650
Fluidbyte <kent@fluidbyte.net>
2012-09-24 08:05:24 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-PHP-jQuery-Terminal-Emulator
-============================
+# PHP-jQuery-Terminal-Emulator
 
-A simple PHP terminal emulator
+A simple PHP terminal emulator using jQuery + PHP. To install simply place on your server and modify the term.php file to set password, default directory, etc.
+
+## Security
+
+Please don't be stupid. Depending on your server setup this could potentially open you up to some major security issues if not properly locked down. The system uses sessions for 
+authentication but that doesn't mean an extra layer of protection wouldn't be a good idea.
---
+++ index.php
@@ -0,0 +1,35 @@
+<!--
+/*
+*  PHP+JQuery Temrinal Emulator by Fluidbyte <http://www.fluidbyte.net>
+*
+*  This software is released as-is with no warranty and is complete free
+*  for use, modification and redistribution
+*/
+-->
+<!doctype html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Terminal</title>
+    <link rel="stylesheet" href="css/screen.css">
+</head>
+
+<body>
+
+    
+    <div id="terminal">
+    
+        <div id="output"></div>
+    
+        <div id="command">
+            <div id="prompt">&gt;</div>
+            <input type="text">
+        </div>
+    
+    </div>
+
+    <script src="js/jquery-1.8.2.js"></script>
+    <script src="js/system.js"></script>
+
+</body>
+</html>
